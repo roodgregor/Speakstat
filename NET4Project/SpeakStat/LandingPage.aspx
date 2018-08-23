805ad72e141a396d50db864b947add290de1e76c
Frances Guce <fcdguce@mymail.mapua.edu.ph>
2018-08-23 14:37:30 +0800

    UI completed.

--- NET4Project/SpeakStat/LandingPage.aspx
+++ NET4Project/SpeakStat/LandingPage.aspx
@@ -8,22 +8,240 @@
     <style type="text/css">
         @font-face {
             font-family: "Walkway Black";
-            src: url("Fonts/Walkway Black.ttf");
-
+            src: url("Fonts/Walkway Black.ttf") format('truetype')
         }
         body
         {
             font-family: 'Walkway Black';
+            margin: 0px;
         }
+        #pageTitle
+        {
+            color: white;
+            font-size: 125px;
+            text-shadow: -4px -4px black, 4px -4px black, -4px 4px black, 4px 4px black;
+            position: absolute;
+            top: 30px;
+            left: 100px;
+        }
+        #bgImage
+        {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            height: 910px;
+            z-index: -1;
+        }
+        .HeadLabels
+        {
+            font-size: 50px;
+            color: white;
+            text-shadow: -2px -2px black, 2px -2px black, -2px 2px black, 2px 2px black;
+            
+        }
+        .SubLabels
+        {
+            font-size: 30px;
+            color: white;
+            text-shadow: -2px -2px black, 2px -2px black, -2px 2px black, 2px 2px black;
+        }
+        #loginpanel
+        {
+            position: absolute;
+            top: 180px;
+            left: 50px;
+            width: 700px;
+            height: 672px;
+            background-color: rgba(255,255,255,0.5);
+            padding: 50px;
+            border-radius: 40px;
+            text-align: center;
+        }
+        #registerpanel
+        {
+            text-align:  center;
+            position: absolute;
+            top: 50px;
+            left: 850px;
+            width: 700px;
+            height: 400px;
+            background-color: rgba(255,255,255,0.5);
+            padding: 50px;
+            border-radius: 40px;
+        }
+        .inputbox
+        {
+            font-size: 30px;
+            border-radius: 20px;
+            width: 95%;
+            height: 95%;
+            border: 2px solid black;
+        }
+        #login, #registerButton
+        {
+            font-family: "Walkway Black";
+            background-color: limegreen;
+            font-size: 30px;
+            border-radius: 20px;
+            padding: 30px;
+            border: 3px solid black;
+            width: 100%;
+            cursor: pointer;
+        }
+
+        #registerButton
+        {
+            background-color: aqua;
+        }
+        
+        #login:hover
+        {
+            background-color: green;
+        }
+
+        #registerButton:hover
+        {
+            background-color: cornflowerblue;
+        }
+
+        .image
+        {            
+            border-radius: 100px;
+            border: 3px solid black;
+        }
+
+        .image:hover
+        {
+            opacity: 0.7;
+        }
+
     </style>
 </head>
 <body>
+    <asp:Image ID="bgImage" runat="server"  ImageUrl="~/Images/BackgroundLanding.jpg" />
     <form id="form1" runat="server">
         <div>
-            <asp:TextBox ID="loginUsername" runat="server" placeholder="juandelacruz" />
-            <asp:TextBox ID="loginPassword" runat="server" placeholder="*****" TextMode="Password" />
-            <asp:Button ID="loginButton" runat="server" Text="Login" />
+            <asp:Label ID="pageTitle" runat="server" Text="SpeakStat"></asp:Label>
+            <table id="loginpanel">
+                <tr>
+                    <td>
+                        <asp:Label CssClass="HeadLabels" ID="Label1" runat="server" Text="Log In To Existing Account"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td><br />
+                        <asp:Label CssClass="SubLabels" ID="Label2" runat="server" Text="Username/Email:"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:TextBox CssClass="inputbox" ID="TextBox1" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td><br />
+                        <asp:Label CssClass="SubLabels" ID="Label3" runat="server" Text="Password:"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:TextBox CssClass="inputbox" ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td><br />
+                        <asp:Button ID="login" runat="server" Text="Log In" />
+                    </td>
+                </tr>
+            </table>
+
+            <table id="registerpanel">
+                <tr>
+                    <td colspan="3">
+                        <asp:Label CssClass="HeadLabels" ID="Label4" runat="server" Text="Register an Account"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <br /><br />
+                        <asp:ImageButton ID="ImageButton1" ImageUrl="~/Images/STUDENT.png" Height="120px" Width="120px" runat="server" CssClass="image" />
+                        <br />
+                        <asp:Label CssClass="SubLabels" ID="Label5" runat="server" Text="STUDENT"></asp:Label>
+                        <!-- insert imagebuttons here-->
+                    </td>
+                    <td>
+                        <br /><br />
+                        <asp:ImageButton ID="ImageButton2" ImageUrl="~/Images/PARENT.png" Height="120px" Width="120px" runat="server" CssClass="image" />
+                        <br />
+                        <asp:Label CssClass="SubLabels" ID="Label6" runat="server" Text="PARENT"></asp:Label>
+                        <!-- insert imagebuttons here-->
+                    </td>
+                    <td>
+                        <br /><br />
+                        <asp:ImageButton ID="ImageButton3" ImageUrl="~/Images/TEACHER.png" Height="120px" Width="120px" runat="server" CssClass="image" />
+                        <br />
+                        <asp:Label CssClass="SubLabels" ID="Label7" runat="server" Text="TEACHER"></asp:Label>
+                        <!-- insert imagebuttons here-->
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <br /><br />
+                       <table>
+                           <tr>
+                                <td colspan="2">
+                                    <asp:Label CssClass="SubLabels" runat="server" ID="Label8" Text="Given Name:" />
+                                    <br />
+                                    <asp:TextBox runat="server" ID="givenName" CssClass="inputbox" />
+                                </td>
+                                <td colspan="2">
+                                    <asp:Label CssClass="SubLabels" runat="server" ID="Label9" Text="Username:" />
+                                    <br />
+                                    <asp:TextBox runat="server" ID="registerUsername" CssClass="inputbox" />
+                                </td>
+                           </tr>
+                           <tr>
+                                <td colspan="2">
+                                    <br /><br />
+                                    <asp:Label CssClass="SubLabels" runat="server" ID="Label10" Text="Last Name:" />
+                                    <br />
+                                    <asp:TextBox runat="server" ID="lastName" CssClass="inputbox" />
+                                </td>
+                                <td colspan="2">
+                                    <br /><br />
+                                    <asp:Label CssClass="SubLabels" runat="server" ID="Label11" Text="Password: " />
+                                    <br />
+                                    <asp:TextBox runat="server" ID="registerPassword" CssClass="inputbox" TextMode="Password" />
+                                </td>
+                           </tr>
+                           <tr>
+                                <td colspan="2">
+                                    <br /><br />
+                                    <asp:Label CssClass="SubLabels" runat="server" ID="Label12" Text="Email Address: " />
+                                    <br />
+                                    <asp:TextBox runat="server" ID="TextBox3" CssClass="inputbox" />
+                                </td>
+                                <td colspan="2">
+                                    <br /><br />
+                                    <asp:Label CssClass="SubLabels" runat="server" ID="Label13" Text="Confirm Password: " />
+                                    <br />
+                                    <asp:TextBox runat="server" ID="TextBox4" CssClass="inputbox" TextMode="Password" />
+                                </td>
+                           </tr>
+                           <tr>
+                               <td colspan="4">
+                                   <br /><br />
+                                   <asp:Button runat="server" Text="Register" ID="registerButton" />
+                               </td>
+                           </tr>
+                       </table>
+                    </td>
+                </tr>
+            </table>
         </div>
     </form>
+    <div id="forLogin" runat="server">
+
+    </div>
 </body>
 </html>
--- NET4Project/SpeakStat/LandingPage.aspx.designer.cs
+++ NET4Project/SpeakStat/LandingPage.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace SpeakStat {
     public partial class LandingPage {
         
         /// <summary>
+        /// bgImage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image bgImage;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -22,30 +31,255 @@ namespace SpeakStat {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// loginUsername control.
+        /// pageTitle control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox loginUsername;
+        protected global::System.Web.UI.WebControls.Label pageTitle;
         
         /// <summary>
-        /// loginPassword control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox loginPassword;
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// loginButton control.
+        /// Label2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button loginButton;
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// login control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button login;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// ImageButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// ImageButton2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton2;
+        
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// ImageButton3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton3;
+        
+        /// <summary>
+        /// Label7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Label8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// givenName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox givenName;
+        
+        /// <summary>
+        /// Label9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+        
+        /// <summary>
+        /// registerUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox registerUsername;
+        
+        /// <summary>
+        /// Label10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+        
+        /// <summary>
+        /// lastName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox lastName;
+        
+        /// <summary>
+        /// Label11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+        
+        /// <summary>
+        /// registerPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox registerPassword;
+        
+        /// <summary>
+        /// Label12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+        
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// Label13 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+        
+        /// <summary>
+        /// TextBox4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        
+        /// <summary>
+        /// registerButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button registerButton;
+        
+        /// <summary>
+        /// forLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl forLogin;
     }
 }
